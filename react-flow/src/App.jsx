1311c34de08630a93c097308ce229ab4b6cce678
Emmanuel Allan <90669430+EmmanuelAllanMJ@users.noreply.github.com>
2023-04-01 00:58:23 +0530

    Basic app

--- react-flow/src/App.jsx
+++ react-flow/src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useCallback } from "react";
 import Canvas from "./components/Canvas/Canvas";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 export default function App() {
   return (
     <>
       <Header />
       <Canvas />
+      <Footer />
     </>
   );
 }
--- react-flow/src/components/Canvas/Canvas.jsx
+++ react-flow/src/components/Canvas/Canvas.jsx
@@ -1,42 +1,20 @@
-import React, { useCallback } from "react";
-import ReactFlow, { useNodesState, useEdgesState, addEdge } from "reactflow";
+import ReactFlow from "reactflow";
 
 import "reactflow/dist/style.css";
 
 const initialNodes = [
   { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
   { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
-  { id: "3", position: { x: 0, y: 200 }, data: { label: "3" } },
 ];
 const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
 
 export default function Canvas() {
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-
-  const onConnect = useCallback(
-    (params) => setEdges((eds) => addEdge(params, eds)),
-    [setEdges]
-  );
-
   return (
     <div
-      style={{
-        border: "2px solid red",
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
+      style={{ width: "80%", height: "50vh" }}
+      className="border-2 block m-auto"
     >
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-      />
+      <ReactFlow nodes={initialNodes} edges={initialEdges} />
     </div>
   );
 }
