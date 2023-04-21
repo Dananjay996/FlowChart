4eb5dbbe412df7b502bcba756ec68700e3b8cc9a
Emmanuel Allan <90669430+EmmanuelAllanMJ@users.noreply.github.com>
2023-04-22 00:57:47 +0530

    Removed react-flow

--- react-flow/src/components/Canvas/Canvas.jsx
+++ react-flow/src/components/Canvas/Canvas.jsx
@@ -1,20 +1,7 @@
-import ReactFlow from "reactflow";
+import React from "react";
 
-import "reactflow/dist/style.css";
-
-const initialNodes = [
-  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
-  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
-];
-const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
-
-export default function Canvas() {
-  return (
-    <div
-      style={{ width: "80%", height: "50vh" }}
-      className="border-2 block m-auto"
-    >
-      <ReactFlow nodes={initialNodes} edges={initialEdges} />
-    </div>
-  );
+function Canvas() {
+  return <div>Canvas</div>;
 }
+
+export default Canvas;
