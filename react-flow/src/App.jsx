a6a51cbcaffd3cc421fea95cce8923bd5c52a9e3
Emmanuel Allan <90669430+EmmanuelAllanMJ@users.noreply.github.com>
2023-04-22 01:42:27 +0530

    Added basic tree using d3

--- react-flow/src/App.jsx
+++ react-flow/src/App.jsx
@@ -2,12 +2,13 @@ import React, { useCallback } from "react";
 import Canvas from "./components/Canvas/Canvas";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import data from "./components/Canvas/data.json";
 
 export default function App() {
   return (
     <>
       <Header />
-      <Canvas />
+      <Canvas data={data} />
       <Footer />
     </>
   );
