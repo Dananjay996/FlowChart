b3adca80a56d444a89632184a3d5fc3dd12a7a22
Emmanuel Allan <90669430+EmmanuelAllanMJ@users.noreply.github.com>
2023-04-25 00:04:22 +0530

    Added pop up to show datas

--- react-flow/src/assets/Hex.jsx
+++ react-flow/src/assets/Hex.jsx
@@ -39,23 +39,36 @@ export default function Hex({ data }) {
 
   return (
     <>
-      {onShowDetails && (
-        <text dx="0" dy="10" fontSize="5">
-          {data.description}
-        </text>
-      )}
       <svg
         version="1.1"
         id="Layer_1"
         xmlns="http://www.w3.org/2000/svg"
         x="-15px"
         y="-15px"
-        viewBox="0 0 125.44 122.88"
-        width="29"
-        height="29"
+        viewBox="0 0 500 500"
+        width="120"
+        height="120"
         onClick={() => setOnShowDetails(!onShowDetails)}
         // onMouseLeave={() => setOnShowDetails(false)}
       >
+        {onShowDetails && (
+          <foreignObject x="100" y="100" width="250" height="250">
+            <div xmlns="http://www.w3.org/1999/xhtml" s>
+              <div class=" bg-blue-500 opacity-0 animate-fade  fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex justify-center items-center transition-opacity duration-300 ease-in-out">
+                <div class="bg-white p-4 rounded-md shadow-md">
+                  <h2 class="text-lg font-semibold mb-2">{data.DisplayName}</h2>
+                  <p class="text-gray-600 mb-4">{data.description}</p>
+                  <button
+                    id="closePopupBtn"
+                    class="bg-gray-500 text-white px-3 py-1 rounded-md hover:bg-gray-600 transition duration-300 ease-in-out"
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </foreignObject>
+        )}
         <path d="M55.06,1.07l51.37,29.66v61.43l-53.22,30.73L0,92.15V30.73L53.22,0L55.06,1.07L55.06,1.07z M76.13,21.74L53.22,8.51 L7.39,34.97v52.94l45.83,26.46l45.83-26.46V34.97L76.13,21.74L76.13,21.74z" />
 
         <polygon
