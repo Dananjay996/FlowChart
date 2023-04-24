1caa161d259544f7f48130c7ba306d249df8f229
Emmanuel Allan <90669430+EmmanuelAllanMJ@users.noreply.github.com>
2023-04-25 00:08:57 +0530

    Minor change

--- react-flow/src/assets/Hex.jsx
+++ react-flow/src/assets/Hex.jsx
@@ -48,7 +48,6 @@ export default function Hex({ data }) {
         viewBox="0 0 500 500"
         width="120"
         height="120"
-        onClick={() => setOnShowDetails(!onShowDetails)}
         // onMouseLeave={() => setOnShowDetails(false)}
       >
         {onShowDetails && (
@@ -72,8 +71,9 @@ export default function Hex({ data }) {
         <path d="M55.06,1.07l51.37,29.66v61.43l-53.22,30.73L0,92.15V30.73L53.22,0L55.06,1.07L55.06,1.07z M76.13,21.74L53.22,8.51 L7.39,34.97v52.94l45.83,26.46l45.83-26.46V34.97L76.13,21.74L76.13,21.74z" />
 
         <polygon
-          points="55 5,105 30,105 90, 50 120,3 90,5 30"
+          points="55 5,100 30,100 90, 50 120,3 90,5 30"
           fill="transparent"
+          onClick={() => setOnShowDetails(!onShowDetails)}
         />
 
         <g onClick={() => showForm()}>
