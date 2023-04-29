094cfaf24b4ab71e91e403da3c5fc106552f8356
Emmanuel Allan <90669430+EmmanuelAllanMJ@users.noreply.github.com>
2023-04-30 00:27:41 +0530

    Basic struct for vertical lines

--- react-flow/src/components/Tree/Links.jsx
+++ react-flow/src/components/Tree/Links.jsx
@@ -1,26 +1,37 @@
 import React from "react";
+import { linkVertical } from "d3-shape";
 
 const linkCustom = ({ source, target }, radius) => {
-  const breakY = source.y + 140;
-  if (source.x === target.x) {
-    return `M${source.y + 15} ${source.x} L${target.y - 16} ${target.x} 
-            M${target.y - 20} ${target.x + 5} L${target.y - 15} ${target.x} 
-            M${target.y - 20} ${target.x - 5} L${target.y - 15} ${target.x}`;
-  } else {
-    const isClockwise = target.x < source.x;
-    return `M${source.y + 15} ${source.x} L${breakY - radius} ${
-      source.x
-    } a${radius},${radius} 0 0 ${isClockwise ? 0 : 1} ${radius},${
-      isClockwise ? -radius : radius
-    } L${breakY} ${
-      isClockwise ? target.x + radius : target.x - radius
-    } a${radius},${radius} 0 0 ${isClockwise ? 1 : 0} ${radius},${
-      isClockwise ? -radius : radius
-    } L${target.y - 16} ${target.x}
-      M${target.y - 20} ${target.x + 5} L${target.y - 15} ${target.x} 
-      M${target.y - 20} ${target.x - 5} L${target.y - 15} ${target.x}`;
-  }
+  const breakX = (source.x + target.x) / 2;
+  const isClockwise = target.y < source.y;
+  return `M${source.x} ${source.y + 15} 
+          L${breakX} ${source.y + 15}
+          a${radius},${radius} 0 0 ${isClockwise ? 0 : 1} ${
+    isClockwise ? radius : -radius
+  },${radius}
+          L${breakX} ${target.y - 15}
+          a${radius},${radius} 0 0 ${isClockwise ? 1 : 0} ${
+    isClockwise ? radius : -radius
+  },${radius}
+          L${target.x} ${target.y - 15}
+          M${target.x - 5} ${target.y - 20} 
+          L${target.x} ${target.y - 15} 
+          L${target.x + 5} ${target.y - 20}`;
 };
+
+// rotate tree layout 90 degrees left
+const rotateLayout = (node) => {
+  return {
+    ...node,
+    x: node.y,
+    y: -node.x,
+  };
+};
+
+// create a vertical link generator
+const linkGenerator = linkVertical()
+  .x((node) => node.y)
+  .y((node) => node.x);
 
 export default (props) => {
   const { links } = props;
@@ -28,7 +39,12 @@ export default (props) => {
     <g className="d3-tree-links">
       {links.map((link, i) => (
         <g key={i}>
-          <path d={linkCustom(link, 8)} />
+          <path
+            d={linkGenerator({
+              source: rotateLayout(link.source),
+              target: rotateLayout(link.target),
+            })}
+          />
         </g>
       ))}
     </g>
