65dda644fdab38f07c4f329ff8168ab236b53c79
Emmanuel Allan <90669430+EmmanuelAllanMJ@users.noreply.github.com>
2023-04-30 00:13:21 +0530

    Removed number of nodes

--- react-flow/src/components/Tree/Nodes.jsx
+++ react-flow/src/components/Tree/Nodes.jsx
@@ -57,42 +57,6 @@ export default (props) => {
                 onClick={() => handlePlusIconClick(node)} // Add onClick event handler to handle click event
               /> */}
             </g>
-
-            {node.children ? (
-              <g>
-                <rect
-                  style={{
-                    fill: "#202525",
-                    strokeWidth: node.children?.length,
-                  }}
-                  className="rect-test"
-                  width="35"
-                  height="20"
-                  rx="5"
-                  x="50"
-                  y="-10"
-                />
-                <line
-                  x1="15"
-                  y1="0"
-                  x2="50"
-                  y2="0"
-                  style={{
-                    fill: "#89a19d",
-                    strokeWidth: node.children?.length,
-                    stroke: "#89a19d",
-                    strokeOpacity: 1,
-                  }}
-                />
-                <text className="rect-text" dx="55" dy="3">
-                  {node.children?.length >= 2
-                    ? `${node.children.length} calls`
-                    : `1 call`}
-                </text>
-              </g>
-            ) : (
-              ""
-            )}
           </g>
         );
       })}
