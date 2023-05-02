0296f3f39396777e4f008cc67b9c703347d8a019
Emmanuel Allan <90669430+EmmanuelAllanMJ@users.noreply.github.com>
2023-05-02 15:09:09 +0530

    Added box and circle and drag and drop box alone and not canvas

--- pages/index.js
+++ pages/index.js
@@ -5,23 +5,46 @@ const Box = () => {
   const [isDragging, setIsDragging] = useState(false);
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
   const [boxPosition, setBoxPosition] = useState({ x: 50, y: 50 });
+  const boxWidth = 100;
+  const boxHeight = 100;
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
-    const boxWidth = 100;
-    const boxHeight = 100;
 
     const drawBox = () => {
+      // loop boxPosition array
+      // for (let i = 0; i < boxPosition.length; i++) {
+      //   ctx.clearRect(0, 0, canvas.width, canvas.height);
+      //   ctx.fillStyle = "red";
+      //   ctx.fillRect(boxPosition[i].x, boxPosition[i].y, boxWidth, boxHeight);
+      //   ctx.strokeStyle = "#000";
+      //   ctx.stroke();
+      // }
+
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       ctx.fillStyle = "red";
       ctx.fillRect(boxPosition.x, boxPosition.y, boxWidth, boxHeight);
+      // draw a circle in bottom middle
+      ctx.beginPath();
+      ctx.arc(
+        boxPosition.x + boxWidth / 2,
+        boxPosition.y + boxHeight,
+        5,
+        0,
+        2 * Math.PI
+      );
+      ctx.fillStyle = "blue";
+      ctx.fill();
+
       ctx.strokeStyle = "#000";
       ctx.stroke();
     };
 
     drawBox();
   }, [boxPosition]);
+
+  // code to move box and circle constantly
 
   const handleMouseDown = (e) => {
     setIsDragging(true);
@@ -33,7 +56,13 @@ const Box = () => {
   };
 
   const handleMouseMove = (e) => {
-    if (isDragging) {
+    if (
+      isDragging &&
+      e.clientX > boxPosition.x &&
+      e.clientY > boxPosition.y &&
+      e.clientX < boxPosition.x + boxWidth &&
+      e.clientY < boxPosition.y + boxHeight
+    ) {
       const deltaX = e.clientX - mousePosition.x;
       const deltaY = e.clientY - mousePosition.y;
       setBoxPosition({
