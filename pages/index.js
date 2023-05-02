fd3c40f6e7ae32b3a284d7ccc1e9614f99bbd664
Emmanuel Allan <90669430+EmmanuelAllanMJ@users.noreply.github.com>
2023-05-02 15:42:03 +0530

    Adding multiple rectangle drag and drop

--- pages/index.js
+++ pages/index.js
@@ -1,89 +1,103 @@
-import { useEffect, useRef, useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
-const Box = () => {
+export default function Canvas() {
   const canvasRef = useRef(null);
-  const [isDragging, setIsDragging] = useState(false);
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-  const [boxPosition, setBoxPosition] = useState({ x: 50, y: 50 });
-  const boxWidth = 100;
-  const boxHeight = 100;
+  const [boxes, setBoxes] = useState([
+    { x: 100, y: 100, width: 100, height: 50 },
+    { x: 300, y: 300, width: 100, height: 50 },
+    { x: 350, y: 350, width: 100, height: 50 },
+  ]);
+  const [selectedBoxIndex, setSelectedBoxIndex] = useState(null);
+  const [mouseOffset, setMouseOffset] = useState({ x: 0, y: 0 });
+
+  const handleMouseDown = (e) => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    const mousePos = getMousePos(canvas, e);
+    for (let i = boxes.length - 1; i >= 0; i--) {
+      const box = boxes[i];
+      if (
+        mousePos.x >= box.x &&
+        mousePos.x <= box.x + box.width &&
+        mousePos.y >= box.y &&
+        mousePos.y <= box.y + box.height
+      ) {
+        setSelectedBoxIndex(i);
+        setMouseOffset({
+          x: mousePos.x - box.x,
+          y: mousePos.y - box.y,
+        });
+        break;
+      }
+    }
+  };
+
+  const handleMouseUp = () => {
+    setSelectedBoxIndex(null);
+  };
+
+  const handleMouseMove = (e) => {
+    if (selectedBoxIndex !== null) {
+      const canvas = canvasRef.current;
+      const mousePos = getMousePos(canvas, e);
+      const newBoxes = [...boxes];
+      const box = newBoxes[selectedBoxIndex];
+      box.x = mousePos.x - mouseOffset.x;
+      box.y = mousePos.y - mouseOffset.y;
+      setBoxes(newBoxes);
+    }
+  };
+
+  const getMousePos = (canvas, e) => {
+    const rect = canvas.getBoundingClientRect();
+    const scaleX = canvas.width / rect.width;
+    const scaleY = canvas.height / rect.height;
+    return {
+      x: (e.clientX - rect.left) * scaleX,
+      y: (e.clientY - rect.top) * scaleY,
+    };
+  };
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#fff";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    // draw circle at bottom middle of rectangle
+    // for (let i = 0; i < boxes.length; i++) {
+    //   const box = boxes[i];
+    // }
 
-    const drawBox = () => {
-      // loop boxPosition array
-      // for (let i = 0; i < boxPosition.length; i++) {
-      //   ctx.clearRect(0, 0, canvas.width, canvas.height);
-      //   ctx.fillStyle = "red";
-      //   ctx.fillRect(boxPosition[i].x, boxPosition[i].y, boxWidth, boxHeight);
-      //   ctx.strokeStyle = "#000";
-      //   ctx.stroke();
-      // }
-
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.fillStyle = "red";
-      ctx.fillRect(boxPosition.x, boxPosition.y, boxWidth, boxHeight);
-      // draw a circle in bottom middle
+    ctx.fillStyle = "#000";
+    for (let i = 0; i < boxes.length; i++) {
+      const box = boxes[i];
+      ctx.fillStyle = "#000";
+      ctx.fillRect(box.x, box.y, box.width, box.height);
+      ctx.fillStyle = "#f00";
       ctx.beginPath();
       ctx.arc(
-        boxPosition.x + boxWidth / 2,
-        boxPosition.y + boxHeight,
+        box.x + box.width / 2,
+        box.y + box.height,
         5,
         0,
-        2 * Math.PI
+        Math.PI * 2,
+        true
       );
-      ctx.fillStyle = "blue";
+      ctx.arc(box.x + box.width / 2, box.y, 5, 0, Math.PI * 2, true);
+      ctx.closePath();
       ctx.fill();
-
-      ctx.strokeStyle = "#000";
-      ctx.stroke();
-    };
-
-    drawBox();
-  }, [boxPosition]);
-
-  // code to move box and circle constantly
-
-  const handleMouseDown = (e) => {
-    setIsDragging(true);
-    setMousePosition({ x: e.clientX, y: e.clientY });
-  };
-
-  const handleMouseUp = () => {
-    setIsDragging(false);
-  };
-
-  const handleMouseMove = (e) => {
-    if (
-      isDragging &&
-      e.clientX > boxPosition.x &&
-      e.clientY > boxPosition.y &&
-      e.clientX < boxPosition.x + boxWidth &&
-      e.clientY < boxPosition.y + boxHeight
-    ) {
-      const deltaX = e.clientX - mousePosition.x;
-      const deltaY = e.clientY - mousePosition.y;
-      setBoxPosition({
-        x: boxPosition.x + deltaX,
-        y: boxPosition.y + deltaY,
-      });
-      setMousePosition({ x: e.clientX, y: e.clientY });
     }
-  };
+  }, [boxes]);
 
   return (
     <canvas
       ref={canvasRef}
-      width={1000}
+      width={500}
       height={500}
+      style={{ border: "1px solid #000" }}
       onMouseDown={handleMouseDown}
       onMouseUp={handleMouseUp}
       onMouseMove={handleMouseMove}
-      className="border-2 border-black "
     />
   );
-};
-
-export default Box;
+}
